ce016549352a1eefccc4962ec1af72d2f213b288
linu-2208 <linithakadiyala@gmail.com>
2025-01-10 13:29:54 +0530

    New commit

--- 13.install-packages.sh
+++ 13.install-packages.sh
@@ -5,7 +5,23 @@ Id=$(id -u)
 R="\e[31m"
 G="\e[32m"
 N="\e[0m"
+Y="\e[33m"
 
+TIMESTAMP=$(date +%F-%H-%S)
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+
+echo "script stareted executing at $TIMESTAMP" &>> LOGFILE
+
+
+VALIDATE(){
+     if [ $1 -ne 0]
+     then
+         echo -e "$2...$R FAiled $N"
+     else
+        echo -e "$2... $G SUCESS $N"
+     fi       
+
+}
 
 if [ $Id -ne 0 ]
 then
@@ -15,4 +31,20 @@ else
     echo "You are root user"
  fi   
 
- echo "All args passed: $@"
+ #echo "All args passed: $@"
+# package =git for first time
+#packages git mysql postfix
+ for package in $@ 
+do 
+
+     Yum list installed $package &>> LOGFILE #Check installed or not
+     if [ $? -ne 0] #if not installed
+     then 
+         yum install $package -y &>> LOGFILE #install the package
+         VALIDATE $? "Installation of $Package "
+     else
+          echo -e   "$Package is already installed.... $Y Skipping $N"
+      fi     
+     done    
+ 
+

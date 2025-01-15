65669ee7d4b19ef38791dbad0738addb54a1b2d3
linu-2208 <linithakadiyala@gmail.com>
2025-01-15 12:06:22 +0530

    shell

--- 15.greetings.sh
+++ 15.greetings.sh
@@ -14,10 +14,19 @@ while getopts ":n:w:h" opt; do
     case $opt in
         n) NAME="$OPTARG";;
         w) WISHES="$OPTARG";;
-        h|*) USAGE; exit;;
+        \?) echo "invalid options: -" $OPTARG"" >&2; USAGE; exit;
+        :) USAGE; exit;;
+        h) USAGE; exit;;
+
      esac   
  done    
 
+if [ -z "$NAME"] || [-z "$WISHES"] ; then
+    echo "Error: Both -n and -w are mandatory options"
+    USAGE
+    exit 1
+fi
 
+ echo " Hello $NAME. $WISHES. I am learning shell script"
 
 
